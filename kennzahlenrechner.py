c87ad6291fcdbff54789386ae32a899eaea15fc5
Jocomol <joelmeier08@gmail.com>
2019-03-13 19:31:38 +0100

    push for commit

--- kennzahlenrechner.py
+++ kennzahlenrechner.py
@@ -9,10 +9,24 @@ class Main():
             with open("./std_kennzahlen.yml", 'r') as stream:
                 self.std_kennzahlen_yaml = yaml.load(stream)
             with open("./bilanz.yml", 'r') as stream:
-                print("fff")
                 self.bilanz_yaml = yaml.load(stream)
         except Exception as e:
             print(colorful.bold_red(e))
+        self.eigenkapitalrent = self.std_kennzahlen_yaml["kennzahlen"]["eigenkapitalrent"]
+        self.gesamtkapitalrent = self.std_kennzahlen_yaml["kennzahlen"]["gesamtkapitalrent"]
+        self.liquidmin_1 = self.std_kennzahlen_yaml["kennzahlen"]["liquidmin_1"]
+        self.liquidmax_1 = self.std_kennzahlen_yaml["kennzahlen"]["liquidmax_1"]
+        self.liquid_2 = self.std_kennzahlen_yaml["kennzahlen"]["liquid_2"]
+        self.liquidmin_3 = self.std_kennzahlen_yaml["kennzahlen"]["liquidmin_3"]
+        self.lagerumschlag = self.std_kennzahlen_yaml["kennzahlen"]["lagerumschlag"]
+        self.debitorenzahlungsfrist = self.std_kennzahlen_yaml["kennzahlen"]["debitorenzahlungsfrist"]
+        self.fremdfinanzierungsgradmin = self.std_kennzahlen_yaml["kennzahlen"]["fremdfinanzierungsgradmin"]
+        self.fremdfinanzierungsgradmax = self.std_kennzahlen_yaml["kennzahlen"]["fremdfinanzierungsgradmax"]
+        self.eigenfinanzierungsgradmin = self.std_kennzahlen_yaml["kennzahlen"]["eigenfinanzierungsgradmin"]
+        self.eigenfinanzierungsgradmax = self.std_kennzahlen_yaml["kennzahlen"]["eigenfinanzierungsgradmax"]
+        self.anlagedeckungsgradmin_1 = self.std_kennzahlen_yaml["kennzahlen"]["anlagedeckungsgradmin_1"]
+        self.anlagedeckungsgradmax_1 = self.std_kennzahlen_yaml["kennzahlen"]["anlagedeckungsgradmax_1"]
+        self.anlagedeckungsgrad_2 = self.std_kennzahlen_yaml["kennzahlen"]["anlagedeckungsgrad_2"]
 
 
 if __name__ == "__main__":
