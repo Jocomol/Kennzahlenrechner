b24292c2e621067128fb5166f4ac9ea7720d699e
Jocomol <joelmeier08@gmail.com>
2019-03-15 16:13:23 +0100

    added check_kennzahl_range

--- kennzahlenrechner.py
+++ kennzahlenrechner.py
@@ -42,13 +42,34 @@ class Main():
         self.selbstfinanzierungsgrad = self.bilanz_yaml["Bemerkungen"]["selbstfinanzierungsgrad"]
 
         # Bilanz
-        self.reingewinn = self.bilanz_yaml["Erfolgsrechnung"]["gewinn"]
-        self.zinsen = self.bilanz_yaml["Erfolgsrechnung"]["Aufwand"]["zinsen"]
-        self.gesamtkapital = self.bilanz_yaml["Bilanz"]["gesamtvermoegen"]
+        self.reingewinn = self.bilanz_yaml["Erfolgsrechnung"]["GEWINN"]
+        self.zinsen = self.bilanz_yaml["Erfolgsrechnung"]["Aufwand"]["ZINSEN"]
+        self.gesamtkapital = self.bilanz_yaml["Bilanz"]["GESAMTVERMOEGEN"]
+        self.liquide_mittel = self.bilanz_yaml["Bilanz"]["Aktiven"]["Umlaufvermoegen"]["LIQUIDE_MITTEL"]
+        forderungen = self.bilanz_yaml["Bilanz"]["Aktiven"]["Umlaufvermoegen"]["DEBITOREN"]
+        self.kurzfristiges_FK = 0
+        for kf_fk in self.bilanz_yaml['Bilanz']['Passiven']['Fremdkapital']['Kurzfristiges_FK'].values():
+            self.kurzfristiges_FK += kf_fk
+        self.eigenkapital = 0
+        for ek in self.bilanz_yaml['Bilanz']['Passiven']['Eigenkapitalkapital'].values():
+             self.eigenkapital += ek
+        self.umlaufvermoegen = 0
+        for uv in self.bilanz_yaml["Bilanz"]["Aktiven"]["Umlaufvermoegen"].values():
+            self.umlaufvermoegen += uv
 
     def validate_bilanz(self):
         print("WIP")
         # TODO
+
+    def check_kennzahl_range(self, min, max, name, zahl):
+        if resultat >= min and resultat <= max:
+            print(name + colorful.green(str(resultat) + "% ") + "[" + colorful.green(OK) + "]" + "Min: " + min + "% Max: " + max + "%")
+        elif resultat < min:
+            print(name + colorful.red(str(resultat) + "% ") + "[" + colorful.red(Zu Tief) + "]" + "Min: " + min + "% Max: " + max + "%")
+        elif resultat > max:
+            print(name + colorful.red(str(resultat) + "% ") + "[" + colorful.red(Zu Hoch) + "]" + "Min: " + min + "% Max: " + max + "%")
+        else:
+            print(name + colorful.red(str(resultat) + "% ") + "[" + colorful.red(ERROR) + "]" + "Min: " + min + "% Max: " + max + "%")
 
     def cal_gesamtkapitalrent(self):
         resultat = (self.reingewinn + self.zinsen) * 100 / self.gesamtkapital
@@ -58,35 +79,31 @@ class Main():
             print(colorful.red("Die Gesamtkapitalrentabilität liegt bei " + str(resultat) + "%. Sie ist damit NICHT gewährleistet."))
 
     def cal_liquiditaetsgrad1(self):
-        liquide_mittel = self.bilanz_yaml["Bilanz"]["Aktiven"]["Umlaufvermoegen"]["liquide_mittel"]
-        kurzfristiges_FK = 0
-        for kf_fk in self.bilanz_yaml['Bilanz']['Passiven']['Fremdkapital']['Kurzfristiges_FK'].values():
-            kurzfristiges_FK = kurzfristiges_FK + kf_fk
-        resultat = liquide_mittel * 100 / kurzfristiges_FK
-        if resultat >= self.liquidmin_1 and resultat <= self.liquidmax_1:
-            print(colorful.green("Der Liquiditätsgrad_1 liegt bei " + str(resultat) + "%."))
-        elif resultat < self.liquidmin_1:
-            print(colorful.red("Der Liquiditätsgrad_1 liegt bei " + str(resultat) + "%. Dies ist zu tief"))
-        elif resultat > self.liquidmin_1:
-            print(colorful.red("Der Liquiditätsgrad_1 liegt bei " + str(resultat) + "%. Dies ist zu hoch"))
-        else:
-            print(colorful.red("Ein Fehler ist unterlaufen"))
+        resultat = self.liquide_mittel * 100 / self.kurzfristiges_FK
+        self.check_kennzahl_range(self.liquidmin_1, self.liquidmax_1, "Liquiditätsgrad 1", resultat)
 
     def cal_liquiditaetsgrad2(self):
-        liquide_mittel = self.bilanz_yaml["Bilanz"]["Aktiven"]["Umlaufvermoegen"]["liquide_mittel"]
-        kurzfristiges_FK = 0
-        for kf_fk in self.bilanz_yaml['Bilanz']['Passiven']['Fremdkapital']['Kurzfristiges_FK'].values():
-            kurzfristiges_FK = kurzfristiges_FK + kf_fk
-        forderungen = self.bilanz_yaml["Bilanz"]["Aktiven"]["Umlaufvermoegen"]["debitoren"]
-        resultat = (liquide_mittel + forderungen) * 100 / kurzfristiges_FK
+        resultat = (self.liquide_mittel + self.forderungen) * 100 / self.kurzfristiges_FK
         if resultat >= self.liquid_2:
             print(colorful.green("Der Liquiditätsgrad_2 liegt bei " + str(resultat) + "%."))
         else:
             print(colorful.red("Der Liquiditätsgrad_2 liegt bei " + str(resultat) + "%. Dies ist zu tief"))
 
+    def cal_liquiditaetsgrad3(self):
+        resultat = self.umlaufvermögen * 100 / self.kurzfristiges_FK
+        self.check_kennzahl_range(self.liquidmin_3, self.liquidmax_3, "Liquiditätsgrad 3", resultat)
+
+    def cal_eigenkapitalrentabilitaet(self):
+        resultat = self.reingewinn * 100 / (self.eigenkapital - self.reingewinn)
+        if resultat >= self.eigenkapitalrent:
+            print(colorful.green("Die Eigenkapitalrentabilität liegt bei " + str(resultat) + "%."))
+        else:
+            print(colorful.red("Die Eigenkapitalrentabilität bei " + str(resultat) + "%. Dies ist zu tief"))
 
 if __name__ == "__main__":
     programm = Main()
     programm.cal_gesamtkapitalrent()
     programm.cal_liquiditaetsgrad1()
     programm.cal_liquiditaetsgrad2()
+    programm.cal_eigenkapitalrentabilitaet()
+    programm.cal_liquiditaetsgrad3()
