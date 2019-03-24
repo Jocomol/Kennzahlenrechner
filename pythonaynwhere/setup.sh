0d2b965d05a12ad7969b2f9ac183b02f7da702a7
Jocomol <joelmeier08@gmail.com>
2019-03-24 21:21:40 +0100

    - Fixes VI:   /dev/null/ to /dev/null

--- pythonaynwhere/setup.sh
+++ pythonaynwhere/setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 folder="/home/Jocomol/kennzahlen_files"
 # Renew folder structure
-rm -r $folder &> /dev/null/
-mkdir $folder &> /dev/null/
+rm -r $folder &> /dev/null
+mkdir $folder &> /dev/null
 
 #Make helpscripts executable
-chmod +X ./* &> /dev/null/
-cp * $folder &> /dev/null/
+chmod +X ./* &> /dev/null
+cp * $folder &> /dev/null
 
 #copy the kennzahlenrechner
-cp kennzahlenrechner_shell.py $folder/.. &> /dev/null/
-cd .. &> /dev/null/
-cp bilanz.yml $folder &> /dev/null/
-cp kennzahlenrechner.py $folder &> /dev/null/
-cp std_kennzahlen.yml $folder &> /dev/null/
+cp kennzahlenrechner_shell.py $folder/.. &> /dev/null
+cd .. &> /dev/null
+cp bilanz.yml $folder &> /dev/null
+cp kennzahlenrechner.py $folder &> /dev/null
+cp std_kennzahlen.yml $folder &> /dev/null
 
 
 #dowload requirements
-pip3 install -r requirements.txt &> /dev/null/
+pip3 install -r requirements.txt &> /dev/null
 
 echo "the git folder can now be removed"
